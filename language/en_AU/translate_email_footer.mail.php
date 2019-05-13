10ccf6e985d103a1bd6245966ec2e61a599a33e4
Frans Ward <Frans.Ward@surfnet.nl>
2019-05-13 15:32:22 +0200

    Update translate_email_footer.mail.php
    Make translation text usable for Dutch audience

--- language/en_AU/translate_email_footer.mail.php
+++ language/en_AU/translate_email_footer.mail.php
@@ -11,12 +11,12 @@ subject: translate email footer, shared by all languages
 
 {alternative:plain}
 
-Translate this email: {raw:translatableemail.link}
+Vertaal deze e-mail: {raw:translatableemail.link}
 
 {alternative:html}
 
 <table style="width:808"><tr><td>
 <p>
-    Translate this email:</br> <a href="{translatableemail.link}">{translatableemail.link}</a>
+    Vertaal deze e-mail:</br> <a href="{translatableemail.link}">{translatableemail.link}</a>
 </p>
 </tr></td></table>
