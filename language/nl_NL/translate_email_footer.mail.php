f64e3bcb99593eb43b30e0be26ed0958118f1fcf
Frans Ward <Frans.Ward@surfnet.nl>
2019-05-13 15:33:32 +0200

    Update translate_email_footer.mail.php

--- language/nl_NL/translate_email_footer.mail.php
+++ language/nl_NL/translate_email_footer.mail.php
@@ -11,12 +11,12 @@ subject: vertaal e-mail voettekst, gedeeld door alle talen
 
 {alternative:plain}
 
-Translate this e-mail: {raw:translatableemail.link}
+Translate this email: {raw:translatableemail.link}
 
 {alternative:html}
 
 <table style="width:808"><tr><td>
 <p>
-    Translate this e-mail:</br> <a href="{translatableemail.link}">{translatableemail.link}</a>
+    Translate this email:</br> <a href="{translatableemail.link}">{translatableemail.link}</a>
 </p>
 </tr></td></table>
