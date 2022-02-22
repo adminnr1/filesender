0d31ddbe5379064d82d922ed126dba49a2611459
Nils Vogels <nils.vogels@surf.nl>
2022-02-22 13:07:41 +0100

    Add mopinion to footer

--- config/templates/surf/footer.php
+++ config/templates/surf/footer.php
@@ -8,6 +8,9 @@
                 //if(Config::get('site_showStats')) $versionDisplay .= $functions->getStats(); // TODO
             ?>
             </div>
+            <!-- Mopinion Pastea.se  start -->
+              <script type="text/javascript">(function(){var id="H2vABiB1k10dbZWtGMUlTzSUJwIkmhjl8q1Qbaxv";var js=document.createElement("script");js.setAttribute("type","text/javascript");js.setAttribute("src","//deploy.mopinion.com/js/pastease.js");js.async=true;document.getElementsByTagName("head")[0].appendChild(js);var t=setInterval(function(){try{Pastease.load(id);clearInterval(t)}catch(e){}},50)})();</script>
+            <!-- Mopinion Pastea.se end -->
         </div>
         
         <!-- Version <?php //echo FileSender_Version::VERSION; ?> -->
