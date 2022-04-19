14d75e6421c4bfce2c3dcd233310ff67a0aa7bf8
Nils Vogels <n.vogels@aves-it.nl>
2022-04-19 11:00:06 +0200

    Add Mopinion js

--- config/templates/surf/header.php
+++ config/templates/surf/header.php
@@ -36,6 +36,10 @@
         <script type="text/javascript" src="{path:filesender-config.js.php}"></script>
         
         <script type="text/javascript" src="{path:rest.php/lang?callback=lang.setTranslations}"></script>
+
+        <!-- Mopinion Pastea.se  start -->
+        <script type="text/javascript">(function(){var id="H2vABiB1k10dbZWtGMUlTzSUJwIkmhjl8q1Qbaxv";var js=document.createElement("script");js.setAttribute("type","text/javascript");js.setAttribute("src","//deploy.mopinion.com/js/pastease.js");js.async=true;document.getElementsByTagName("head")[0].appendChild(js);var t=setInterval(function(){try{Pastease.load(id);clearInterval(t)}catch(e){}},50)})();</script>
+        <!-- Mopinion Pastea.se end -->
         
         <meta name="robots" content="noindex, nofollow" />
         
