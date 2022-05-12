d3e206be8451ba106a3beb67a9a3c21da2030746
Nils Vogels <n.vogels@aves-it.nl>
2022-05-12 10:48:57 +0200

    Add mopionion in base tree as well

--- templates/header.php
+++ templates/header.php
@@ -36,6 +36,10 @@
         <script type="text/javascript" src="{path:filesender-config.js.php}"></script>
         
         <script type="text/javascript" src="{path:rest.php/lang?callback=lang.setTranslations}"></script>
+
+        <!-- Mopinion Pastea.se  start -->
+        <script type="text/javascript">(function(){var id="H2vABiB1k10dbZWtGMUlTzSUJwIkmhjl8q1Qbaxv";var js=document.createElement("script");js.setAttribute("type","text/javascript");js.setAttribute("src","//deploy.mopinion.com/js/pastease.js");js.async=true;document.getElementsByTagName("head")[0].appendChild(js);var t=setInterval(function(){try{Pastease.load(id);clearInterval(t)}catch(e){}},50)})();</script>
+        <!-- Mopinion Pastea.se end -->
         
         <meta name="robots" content="noindex, nofollow" />
         
@@ -68,7 +72,7 @@
                                 $opts[] = '<option value="'.$id.'" '.$selected.'>'.Utilities::sanitizeOutput($dfn['name']).'</option>';
                             }
                         
-                            echo '<li><label>'.Lang::tr('user_lang').'</label><select id="language_selector">'.implode('', $opts).'</select></li>';
+                            echo '<li><label>'.Lang::tr('user_lang').' </label><select id="language_selector">'.implode('', $opts).'</select></li>';
                             echo '</ul></div></div>';
                         }
                         ?>
