33ce24822a1336fd962a627a46adad7d3bbbe1a4
Nils Vogels <n.vogels@aves-it.nl>
2022-06-30 13:58:14 +0200

    Fix AUP version to fit database constraints

--- config/language/en_AU/lang.php
+++ config/language/en_AU/lang.php
@@ -5,4 +5,5 @@
 ?>
 <?php
 $lang['service_aup_body_version_1'] = '<a href="https://www.surf.nl/en/code-of-conduct-surffilesender" target="_blank">I accept the following code of conduct when using this service.</a><p>';
+$lang['service_aup_body_version_5'] = '<a href="https://www.surf.nl/en/code-of-conduct-surffilesender" target="_blank">I accept the following code of conduct when using this service.</a><p>';
 $lang['service_aup_header'] = '{cfg:site_name} code of conduct';
